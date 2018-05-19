ff5c43ba5619965c63678ee31069dedda2e4d265
alex_mac <alexey.rozhin90@gmail.com>
2018-05-19 16:48:16 +0300

    eject

--- src/components/AddCite/index.js
+++ src/components/AddCite/index.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import styles from "./index.css";
 
 export default class AddCite extends Component {
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div className={styles.window}>
+        <div className={styles.h1}>Add cite</div>
+        <form method="POST" action="/cites/add">
+          <label className={styles.label} for="cite">
+            Cite
+          </label>
+          <span className={styles.textarea} id="cite" contenteditable="true" />
+          <div className={styles.textareaRectWrapper}>
+            <div className={styles.textareaRect} />
+          </div>
+          <label className={styles.label} for="author">
+            Author
+          </label>
+        </form>
+      </div>
+    );
   }
 }
