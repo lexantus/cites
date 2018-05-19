7362c84b63681983005f731273fe7f5938121357
alex_mac <alexey.rozhin90@gmail.com>
2018-05-19 14:38:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import AddCite from "./components/AddCite";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AddCite />
+      </div>
+    );
+  }
+}
+
+export default App;
