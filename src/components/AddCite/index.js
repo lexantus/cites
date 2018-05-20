073df045eae7cb6d673df1ff7c47d6b561685518
alex_mac <alexey.rozhin90@gmail.com>
2018-05-20 12:50:27 +0300

    style AddCite window

--- src/components/AddCite/index.js
+++ src/components/AddCite/index.js
@@ -17,39 +17,59 @@ export default class AddCite extends Component {
           <label className={styles.label} for="author">
             Author
           </label>
+          <div>
+            <div className={styles.trigger_options} tabIndex="2">
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+            </div>
+            <div className={(styles.options, styles.options_hide)}>
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+              <div className={styles.option}>
+                <img src="pushkin.jpg" width="44" height="44" alt="" />
+                <span className={styles.option__txt}>
+                  Пушкин Александр Сергеич
+                </span>
+              </div>
+            </div>
+            <button className={styles.btn}>Add</button>
+          </div>
+          <button className={styles.submit} type="submit">
+            Submit
+          </button>
         </form>
-        <div className={styles.trigger_options} tabIndex="2">
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Новая карта</span>
-          </div>
-        </div>
-        <div className={styles.options}>
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Пушкин Александр Сергеич</span>
-          </div>
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Пушкин Александр Сергеич</span>
-          </div>
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Пушкин Александр Сергеич</span>
-          </div>
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Пушкин Александр Сергеич</span>
-          </div>
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Пушкин Александр Сергеич</span>
-          </div>
-          <div className={styles.option}>
-            <img src="pushkin.jpg" width="44" height="44" alt="" />
-            <span className={styles.option__txt}>Пушкин Александр Сергеич</span>
-          </div>
-        </div>
       </div>
     );
   }
