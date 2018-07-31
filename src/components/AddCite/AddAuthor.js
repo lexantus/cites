b76cafc870894100273e087695eb14f8b868e24d
alex_mac <alexey.rozhin90@gmail.com>
2018-07-31 09:37:32 +0300

    add author input file and auto focus element when click on add btn

--- src/components/AddCite/AddAuthor.js
+++ src/components/AddCite/AddAuthor.js
@@ -14,6 +14,7 @@ export default class AddAuthor extends Component {
     this.setState({
       isClicked: !this.state.isClicked
     });
+    this.textInput.focus();
   }
   render() {
     const wrapperClass = classNames({
@@ -22,20 +23,29 @@ export default class AddAuthor extends Component {
     });
     return (
       <div className={wrapperClass}>
-        <a
-          className={styles.img}
-          href="/uploadImg"
-          onClick={this.props.uploadImg}
-        >
+        <label className={styles.input_file_label} htmlFor="inputAvatar">
           <img src="placeholder.svg" alt="" />
-        </a>
-        <input className={styles.input} type="text" placeholder="Author name" />
+        </label>
+        <input
+          id="inputAvatar"
+          type="file"
+          accept="image/jpeg, image/png"
+          className={styles.input_file}
+          onClick={this.props.uploadImg}
+        />
+        <input
+          ref={input => (this.textInput = input)}
+          className={styles.input}
+          type="text"
+          placeholder="Author name"
+          autoFocus={this.state.isClicked}
+        />
         <button
           type="button"
           className={styles.btn}
           onClick={this.clickHandler}
         >
-          Add >
+          {this.state.isClicked ? "Add" : "Add >"}
         </button>
       </div>
     );
