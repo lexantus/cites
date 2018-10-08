95d12e8862896849deaa7609530673501dfee448
alex_mac <alexey.rozhin90@gmail.com>
2018-10-08 23:57:56 +0300

    signin ajax

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,11 +3,57 @@ import { Link } from "react-router-dom";
 import styles from "./index.css";
 
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: "",
+      password: ""
+    };
+    this.refLogin = React.createRef();
+    this.refPassword = React.createRef();
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { login, password } = this.state;
+    let isValid = true;
+    if (!login) {
+      this.refLogin.current.classList.add(styles.error);
+      isValid = false;
+    } else {
+      this.refLogin.current.classList.remove(styles.error);
+    }
+    if (!password) {
+      this.refPassword.current.classList.add(styles.error);
+      isValid = false;
+    } else {
+      this.refPassword.current.classList.remove(styles.error);
+    }
+
+    if (isValid) {
+      fetch("/signin", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ login, password })
+      })
+        .then(res => console.log("res", res))
+        .then(data => console.log("data", data))
+        .catch(err => console.log("err", err));
+    }
+  }
+  handleLogin(e) {
+    this.setState({ login: e.target.value });
+  }
+  handlePassword(e) {
+    this.setState({ password: e.target.value });
+  }
   render() {
     return (
       <div>
         <div className={styles.wrapper}>
-          <form className={styles.window} action="/api/login" method="post">
+          <form className={styles.window} action="/signin" method="post">
             <h2 className={styles.header}>Authorization</h2>
             <div className={styles.linkDiv}>
               or <Link to="/register">Register</Link>
@@ -20,6 +66,9 @@ class LoginPage extends Component {
               type="text"
               name="login"
               id="login"
+              ref={this.refLogin}
+              value={this.state.login}
+              onChange={e => this.handleLogin(e)}
               autoComplete="off"
             />
             <label className={styles.label} htmlFor="password">
@@ -30,6 +79,9 @@ class LoginPage extends Component {
               type="password"
               name="password"
               id="password"
+              ref={this.refPassword}
+              value={this.state.password}
+              onChange={e => this.handlePassword(e)}
               autoComplete="off"
             />
             <button className={styles.submit} type="submit">
